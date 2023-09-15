5e6316a5e48fe8c7546ee3134c426278bb48df7b
DTB2324 <dotrongbinh20012004@gmail.com>
2023-09-15 15:14:01 +0700

    first commit

--- UF.java
+++ UF.java
@@ -4,14 +4,17 @@ import edu.princeton.cs.algs4.StdOut;
 public class UF {
     public static void main(String[] args) {
         int N = StdIn.readInt();
-        WeightedQuickUnion uf = new WeightedQuickUnion(N);
+        int ans = N;
+        UFNew uf = new UFNew(N);
         while(!StdIn.isEmpty()){
             int p = StdIn.readInt();
             int q = StdIn.readInt();
             if(!uf.connected(p, q)){
                 uf.union(p, q);
+                ans--;
                 StdOut.println(p + " " + q);
             }
         }
+        StdOut.println(ans);
     }
 }
