5ef92167867fbe018a5470cb8df68cb4b2928659
DTB2324 <dotrongbinh20012004@gmail.com>
2023-10-26 00:18:29 +0700

    2610

--- Week6_7/MergeSort/MergeSort.java
+++ Week6_7/MergeSort/MergeSort.java
@@ -1,16 +1,43 @@
 package Week6_7.MergeSort;
 
 public class MergeSort {
-    public void merge(int[] a, int[] b, int l, int h) {
-        for(int i = 0; i < a.length; i++) {
-            b[i] = a[i];
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
         }
-        if(l <= h) {
-            return;
+        int i = lo, j = mid + 1;
+        for(int k = lo; k <= hi; k++) {
+            if(i > mid)                     a[k] = aux[j++];
+            else if(j > hi)                 a[k] = aux[i++];
+            else if(less(aux[j], aux[i]))  a[k] = aux[j++];
+            else                            a[k] = aux[i++];
         }
     }
 
-    public void sort(int[] a, int l, int h) {
+    public static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        if(hi <= lo)    return;
+        int mid = (hi - lo) / 2 + lo;
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
+    }
 
+    public static void sort(Comparable[] a) {
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, aux, 0, a.length - 1);
+        for(Comparable x : a) {
+            System.out.print(x + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        MergeSort mergeSort = new MergeSort();
+        Integer[] a = {1, 55, 21, 17, 8, 4, 3, 10};
+        mergeSort.sort(a);
     }
 }
